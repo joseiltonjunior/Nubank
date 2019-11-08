b7e0ddcbdcc964d9e7ba77cb91ef1d6423133edb
joseiltonjunior <joseiltonjuniortech@gmail.com>
2019-11-08 20:03:39 -0300

    pausa pro café

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import Header from '~/components/Header';
+
+import { Container } from './styles';
 
 export default function Main() {
   return (
-    <View />
+    <Container>
+      <Header />
+    </Container>
   );
 }

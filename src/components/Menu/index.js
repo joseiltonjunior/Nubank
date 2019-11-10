9ae35c74ed6bbcfce83658c6d956ea41b97d039d
joseiltonjunior <joseiltonjuniortech@gmail.com>
2019-11-10 12:57:32 -0300

    new att
    novas funcionalidades e estilização.
    
    continuaa...

---
+++ src/components/Menu/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import QRCode from 'react-native-qrcode-svg';
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import { Container, Code, Nav, NavItem, NavText, SingOutButtom, SingOutButtomText } from './styles';
+
+export default function Menu() {
+    return (
+        <Container>
+            <Code>
+                <QRCode
+                    value="https://www.linkedin.com/in/junior-ferreira-318989189/"
+                    size={80}
+                    color="#FFF"
+                    backgroundColor="#8B10AE"
+                />
+            </Code>
+
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF" />
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF" />
+                    <NavText>Pefil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF" />
+                    <NavText>Configurar cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF" />
+                    <NavText>Configuração do app</NavText>
+                </NavItem>
+            </Nav>
+
+            <SingOutButtom onPress={() => { }}>
+                <SingOutButtomText>SAIR DO APP</SingOutButtomText>
+            </SingOutButtom>
+        </Container>
+    );
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 
-import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
+import {
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation
+} from './styles';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  function onHandlerStateChange(event) {
+
+  }
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666"/>
-            <Icon name="visibility-off" size={28} color="#666"/>
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 145.050,40</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 350,00 recebida de Junior Ferreira hoje às 18:00;
+        <Menu />
+
+        <PanGestureHandler
+          onGestureEvent={}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 145.050,40</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 350,00 recebida de Junior Ferreira hoje às 18:00;
             </Annotation>
-          </CardFooter>
-        </Card>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
       </Content>
 
       <Tabs />
